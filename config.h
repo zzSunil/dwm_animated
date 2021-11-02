908942ec88be824b862bad08ee30f51344da0b80
Zzheng <2385747226@qq.com>
2021-11-02 18:59:46 +0800

    change border transparence

--- config.h
+++ config.h
@@ -42,7 +42,7 @@ static const char col_gruvred[] = "#cc241d";
 static const char col_cyan[] = "#282828";
 static const char col_border[] = "#cc241d";
 static const unsigned int baralpha = 0xff;
-static const unsigned int borderalpha = 0xff;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray5, col_cyan, col_gray2},
